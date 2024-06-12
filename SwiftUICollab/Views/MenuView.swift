8808069a34fa517a2de6462d947feb7e6a687e0e
Kharatishvilix <Kharatishvililuka@gmail.com>
2024-06-12 20:54:04 +0400

    Add main page view, reusable components.

---
+++ SwiftUICollab/Views/MenuView.swift
@@ -0,0 +1,47 @@
+//
+//  MenuView.swift
+//  Weather
+//
+//  Created by Luka  Kharatishvili on 12.06.24.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    var body: some View {
+        var savedLocations = ["first", "second", "third"]
+        return Menu {
+            ForEach(savedLocations, id: \.self) { option in
+                Button(action: {
+                    // Your action here
+                }) {
+                    Label(option, systemImage: "checkmark")
+                }
+            }
+            NavigationLink(destination: NewLocation()) {
+                Label("Add new location", systemImage: "location.fill")
+            }
+        } label: {
+            HStack {
+                Image("location")
+                Text("Tbilisi")
+                    .font(.system(size: 18))
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+                Image("dropDown")
+            }
+            .padding([.trailing, .leading], 14)
+            .padding(.trailing, 29)
+            .padding([.top, .bottom], 5)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color(UIColor(named: "color") ?? .clear))
+                    .glassmorphism(blurStyle: .systemUltraThinMaterial, backgroundColor: Color(UIColor(named: "color") ?? .clear), cornerRadius: 20, blurOpacity: 0.4)
+            )
+        }
+    }
+}
+
+#Preview {
+    MenuView()
+}
--- SwiftUICollab/Views/WeatherView.swift
+++ SwiftUICollab/Views/WeatherView.swift
@@ -9,13 +9,55 @@ import SwiftUI
 
 struct WeatherView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ZStack {
+                GeometryReader { geometry in
+                    MenuView()
+                        .frame(width: 170, height: 36)
+                        .offset(x: geometry.size.width - 140, y: geometry.safeAreaInsets.top - 36)
+                        .ignoresSafeArea()
+                }
+
+                Image("Sun")
+                    .resizable()
+                    .frame(width: 138, height: 138)
+                    .scaledToFit()
+                    .position(x: 100, y: 100)
+
+                ScrollView {
+                    VStack(alignment: .center) {
+                        Text("30º")
+                            .font(.system(size: 64))
+                            .fontWeight(.semibold)
+                        Text("Precipitations")
+                        Text("Max.: 34º   Min.: 28º")
+                            .padding(.bottom, 20)
+                    }
+                    .glassmorphism(blurStyle: .systemUltraThinMaterial, backgroundColor: Color(UIColor(named: "color") ?? .clear), cornerRadius: 20, blurOpacity: 0.4)
+
+                    .padding(.leading, 13)
+                    .padding(.trailing, 20)
+                    .padding(.bottom, 20)
+                    .foregroundStyle(Color.white)
+
+                    ExtractedView2()
+
+                    ExtractedView(weekDay: "Monday", image: "Image", minTemp: "31°C", maxTemp: "27°C")
+                }
+                .padding(.top, 90)
+            }
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color("sunnyTop"), Color("sunnyBottom")]),
+                               startPoint: .top,
+                               endPoint: .bottom)
+            )
+
+            .background(
+                LinearGradient(gradient: Gradient(colors: [Color("rainyTop"), Color("rainyBottom")]),
+                               startPoint: .top,
+                               endPoint: .bottom)
+            )
         }
-        .padding()
     }
 }
 
